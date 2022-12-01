fbf10379ba8c9bf7f855530498e0ac1bf941c5a3
Vojtěch Musílek <vojtechmusilekk@gmail.com>
2022-12-01 21:26:48 +0100

    clearing duplicate items in history

--- components/editor-func-history.js
+++ components/editor-func-history.js
@@ -1,7 +1,7 @@
 class EditorFuncHistory {
   onChange() {
-    $(".domkit-Box .dom-style-autogen-0").each((index, node) => {
-      var cl = $(node).parent().parent().attr('class');
+    $(".domkit-Box .dom-style-autogen-0").each((_, node) => {
+      let cl = $(node).parent().parent().attr('class');
       if (cl != "domkit-Box") return;
       
       this._addCurrentFunc(node, node.children);
@@ -9,29 +9,41 @@ class EditorFuncHistory {
   }
   
   _addCurrentFunc(parent, nodes) {
-    var maxHistory = getOptionNum("maxHistory", 3);
+    let maxHistory = getOptionNum("maxHistory", 3);
     if (maxHistory == 0) return;
     
-    for (var node of nodes) {
+    for (let node of nodes) {
       if ($(node).css("background").includes("rgb(214, 234, 255)")) {
-        var spanElem = $(node).find("span").get(0);
-        var funcName = $(spanElem).html();
-        
-        var history = $(parent).parent().children(".skyspark-tweaks-func-history");
-        if (history.length > 0 && $(history[0]).html() == funcName) break;
+        let spanElem = $(node).find("span").get(0);
+        let funcHtml = $(spanElem).html();
+        let funcName = $(spanElem).text();
+
+        this._clearDuplicate(funcName);
+
+        let history = $(parent).parent().children(".skyspark-tweaks-func-history");
+        if (history.length > 0 && $(history[0]).html() == funcHtml) break;
         if (history.length > maxHistory - 1) {
           for (let index = maxHistory - 1; index < history.length; index++) {
             history[index].remove();
           }
         }
         
-        var classes = "domkit-control domkit-Label skyspark-tweaks-func-history";
-        var style = "color: #555; width: 100%; display: block; overflow: hidden; " +
+        let classes = "domkit-control domkit-Label skyspark-tweaks-func-history";
+        let style = "color: #555; width: 100%; display: block; overflow: hidden; " +
           "background: #fff; border: 1px solid #bbb; user-select: all; padding: 4px 4px; border-top-style: hidden;";
         
-        $(parent).after(`<span class="${classes}" style="${style}">${funcName}</span>`);
+        $(parent).after(`<span class="${classes}" style="${style}">${funcHtml}</span>`);
         
         break;
+      }
+    }
+  }
+
+  _clearDuplicate(name) {
+    let itemsInHistory = $(".skyspark-tweaks-func-history");
+    for (let node of itemsInHistory) {
+      if ($(node).text() == name) {
+        $(node).detach();
       }
     }
   }
